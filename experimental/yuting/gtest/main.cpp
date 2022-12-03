dabb64db561e65365d010b005d2ea5b59f7ac4ad
lovelydett <xyt@bupt.cn>
2022-12-03 16:08:57 +0800

    [Yuting] init cmake config for GTest

---
+++ experimental/yuting/gtest/main.cpp
@@ -0,0 +1,11 @@
+// Test GTest
+// Created by Yuting Xie on 03/12/2022.
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, GTest!\n";
+
+    return 0;
+}
