7f6b44d5fce589ae626f4922107d25d4e182fddf
cristiano <cch65659185@gmail.com>
2023-03-03 17:24:05 +0800

    cristiano

---
+++ experimental/Cristiano/try_comm/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Cristiano毛🐷 on 24/02/2023.
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <unistd.h>
+#include <serial/serial.h>
+
+int main(){
+  std::cout << "hello world!" << std::endl;
+
+}
+
+//
