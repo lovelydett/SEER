e8614c882ef5c256e640d4d9060a8c1c3bc6eca0
zzf <mc25975@um.edu.mo>
2023-03-07 19:59:38 +0800

    first commit

--- experimental/zifeng/try_serial_comm/main.cc
+++ experimental/zifeng/try_serial_comm/main.cc
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <cassert>
+
 #include "serial/serial.h"
 
 int main() {
@@ -27,33 +29,49 @@ int main() {
         std::cout << "Failed to open serial port." << std::endl;
         return  1;
     }
-//
-//    std::string message = "Hello, this is a message.";
-//    mother_board.write(message);
-//
-//    std::string response = mother_board.readline();
-//    std::cout << "Hello, this is a response: " << response << std::endl;
-//
-//    char data[8] = "Hi baby";
+
+    std::string message = "Hello, this is a message.";
+    auto control = static_cast<uint8_t *>(malloc(1024));
+    int data_len = 12;
+    *(uint8_t*)(control) = 0x5A;
+    *(uint8_t*)(control + 1) = data_len;
+    *(uint8_t*)(control + 2) = 1;
+    *(uint8_t*)(control + 3) = 1;
+    *(short*)(control + 4) = 0x0064;
+    *(short*)(control + 6) = 0x0000;
+    *(short*)(control + 8) = 0x0000;
+    *(uint8_t*)(control + 10) = 0x00;
+    *(uint8_t*)(control + 11) = 0xFF;
+
+    // 345(7) = 3 * 7 * 7 + 4 * 7 + 5 * 1
+    // 1F4(16) = 1 * 16 * 16 + 15 * 16 + 4 * 1
+    // 01000001(2) = 64 + 1
+
+    int total_len = 1 + 1 + 1 + 1 + data_len + 1 + 1;
+
+    auto ret = mother_board.write((uint8_t*)control, total_len);
+    assert(ret == total_len);
+
+    std::string response = mother_board.readline();
+    std::cout << "Hello, this is a response: " << response << std::endl;
+
+//    uint8_t data[8] = "Hi baby";
 //    mother_board.write(data);
+
+    uint8_t received_data[8];
+    mother_board.read(reinterpret_cast<uint8_t*>(received_data), sizeof(received_data));
+    std::cout << "Received data: ";
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << received_data[i] << " ";
+    }
+    std:: cout << std::endl;
+
+    mother_board.close();
+    return 0;
+//    serial::Serial Construct data packet
 //
-//    char received_data[8];
-//    mother_board.read(reinterpret_cast<uint8_t*>(received_data), sizeof(received_data));
-//    std::cout << "Received data: ";
-//    for (int i = 0; i < 8; i++)
-//    {
-//        std::cout << received_data[i] << " ";
-//    }
-//    std:: cout << std::endl;
-//
-//    mother_board.close();
-//    return 0;
-
-
-    // Construct data packet
-
-    //
-    //char data[1000] = "Hello, this is new data.";
-    //mother_board.write(data);
-    printf("hhh");
+//    uint8_t data[1000] = "Hello, this is new data.";
+//    mother_board.write(data);
+//    printf("hhh");
 }
