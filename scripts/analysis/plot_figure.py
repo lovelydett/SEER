59e76434e3c5080058d58da81599ca3ac803884c
lovelydett <xyt@bupt.cn>
2023-04-14 13:58:33 +0800

    [Yuting] new pallet

--- scripts/analysis/plot_figure.py
+++ scripts/analysis/plot_figure.py
@@ -7,6 +7,16 @@ import matplotlib.pyplot as plt
 working_dir = os.path.dirname(__file__)
 os.chdir(working_dir)
 
+# Set plotting theme
+sns.set_theme(style="whitegrid", font_scale=1.4)
+
+PALLET1 = ['#2878b5', '#9ac9db', '#f8ac8c', '#c82423', '#ff8884', '#96c37d', '#c497b2', '#f3d266']
+color_idx = 0
+def next_color():
+    global color_idx
+    color_idx += 1
+    return PALLET1[color_idx % len(PALLET1)]
+
 # Motivation figures
 def plot_bf_adding_active_tasks():
     # Declaring a figure "gnt"
@@ -31,16 +41,16 @@ def plot_bf_adding_active_tasks():
     gnt.grid(True)
     
     # Declaring a bar in schedule
-    gnt.broken_barh([(40, 50)], (30, 9), facecolors =('tab:orange'))
+    gnt.broken_barh([(40, 50)], (30, 9), facecolors = next_color())
     
     # Declaring multiple bars in at same level and same width
-    gnt.broken_barh([(110, 10), (150, 10)], (10, 9),
-                            facecolors ='tab:blue')
+    gnt.broken_barh([(110, 10), (150, 10)], (10, 9), facecolors = next_color())
     
     gnt.broken_barh([(10, 50), (100, 20), (130, 10)], (20, 9),
-                                    facecolors =('tab:red'))
+                                    facecolors = next_color())
+    # gnt.broken_barh([(10, 50), (100, 20), (130, 10)], (20, 9))
     
-    plt.savefig("gantt1.png")
+    plt.show()
 
 # Evaluation figures 
 def plot_core_utilization_rate():
@@ -59,6 +69,7 @@ def plot_core_utilization_rate():
     plt.show()
     
 if __name__ == "__main__":
-    plot_core_utilization_rate()
+    # plot_core_utilization_rate()
+    plot_bf_adding_active_tasks()
     
     
