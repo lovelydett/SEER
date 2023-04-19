8bcfe13987555b88a51a1ca9b9b9a35d5be7911a
lovelydett <xyt@bupt.cn>
2023-04-19 15:27:35 +0800

    [Yuting] more plots

--- scripts/analysis/plot_figure.py
+++ scripts/analysis/plot_figure.py
@@ -10,7 +10,23 @@ working_dir = os.path.dirname(__file__)
 os.chdir(working_dir)
 
 # Set plotting theme
-sns.set_theme(style="whitegrid", font_scale=1.4)
+sns.set_style("whitegrid")
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams["font.size"] = 12
+plt.rcParams["font.weight"] = 'bold'
+sns.set_context("paper", font_scale=1.5, rc={"lines.linewidth": 2.5})
+
+# Set global matplotlib color palette
+color_name = "tab10"
+
+SAVE_FIG = False
+
+# Show color palette
+def show_color_palette():
+    color_name = "tab10"
+    fig, ax = plt.subplots(figsize=(10, 0.5))
+    fig.colorbar(plt.cm.ScalarMappable(cmap=color_name), cax=ax, orientation='horizontal')
+    plt.show()
 
 # Motivation figures
 def plot_time_utilization_fps():
@@ -21,16 +37,25 @@ def plot_time_utilization_fps():
     fig= plt.figure()
     ax = fig.add_subplot(111)
     ax.set_ylim((0, 100))
+    ax.set_xlim((0, 2000))
     # sns.barplot(data=df, x='timestamp', y='utilization', width=100, color=next_color())
     ax.bar(x = df['timestamp'], height = df['utilization'], width=100, color='gray')
 
     ax2 = ax.twinx()
-    ax2.set_ylim((0, 180))
-    sns.lineplot(data=df, x='timestamp', y='e2e', color=next_color(), linewidth=3,  marker='o', markersize=10, ax=ax2)
+    ax2.set_ylim((0, 15))
+    sns.lineplot(data=df, x='timestamp', y='fps', color=next_color(), linewidth=3,  marker='o', markersize=10, ax=ax2)
     # ax2.plot(df['timestamp'], df['fps'], color = next_color())
 
-    plt.show()
+    ax.set_xlabel("Time (ms)")
+    ax.set_ylabel("Utilization (%)")
+    ax2.set_ylabel("Response rate (Hz)")
 
+    if SAVE_FIG:
+        plt.savefig("./result_img/time_utilization_fps.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/time_utilization_fps.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
 
 def plot_bf_adding_active_tasks():
     # Declaring a figure "gnt"
@@ -43,7 +68,7 @@ def plot_bf_adding_active_tasks():
     gnt.set_xlim(0, 160)
     
     # Setting labels for x-axis and y-axis
-    gnt.set_xlabel('seconds since start')
+    gnt.set_xlabel('Seconds since start')
     gnt.set_ylabel('Processor')
     
     # Setting ticks on y-axis
@@ -64,14 +89,34 @@ def plot_bf_adding_active_tasks():
                                     facecolors = next_color())
     # gnt.broken_barh([(10, 50), (100, 20), (130, 10)], (20, 9))
     
-    plt.show()
+    if SAVE_FIG:
+        plt.savefig("./result_img/bf_adding_active_tasks.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/bf_adding_active_tasks.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
 
 def plot_utilization_wait_fps_thermal():
     df = pd.read_csv("./data/time_utilization_fps.csv")
     df = df.iloc[1:]
-    df['e2e'] = 600 / df['fps']
+    df['e2e'] = 100 / df['fps']
+    # Do map to every element in the column
+    df['e2e'] = df['e2e'] ** 3 / 100
+
     plt.scatter(data=df, x='utilization', y='wait', s='e2e')
-    plt.show()
+
+    plt.xlabel("Utilization (%)")
+    plt.ylabel("Sum of wait time (ms)")
+    plt.text(0.05, 0.85, 'Dot radius:\nResponse interval (ms)', ha='left', va='top', transform=plt.gca().transAxes)
+
+    if SAVE_FIG:
+        plt.savefig("./result_img/utilization_wait_fps_thermal.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/utilization_wait_fps_thermal.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+
+    # Clear figure
+    plt.clf()
 
 def plot_speed_sight_distance():
     df = pd.DataFrame(columns=['speed', 'sight_distance', 'e2e'])
@@ -80,31 +125,61 @@ def plot_speed_sight_distance():
     for speed_type, v in SPEED.items():
         for e2e in range(100, 520, 20):
             s_distance = (0.278 * (e2e / 1000) * v) + v * v / (254 * friction)
-            df = df.append({'speed': speed_type, 'sight_distance': s_distance, 'e2e': e2e}, ignore_index=True)
+            # df = df.append({'speed': speed_type, 'sight_distance': s_distance, 'e2e': e2e}, ignore_index=True)
+            df.loc[len(df)] = [speed_type, s_distance, e2e]
     sns.lineplot(data=df, x='e2e', y='sight_distance', hue='speed', markers=True, style='speed', markersize=8, linewidth=3)
-    plt.xlabel("Reaction time (ms)")
+    plt.xlabel("Response time (ms)")
     plt.ylabel("Sight distance (m)")
     plt.tight_layout()
-    plt.show()
+
+    if SAVE_FIG:
+        plt.savefig("./result_img/speed_sight_distance.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/speed_sight_distance.png", bbox_inches='tight', dpi=300)
+
+    else:
+        plt.show()
+    plt.clf()
 
 def plot_cache_miss_execution_time():
     df = pd.read_csv("./data/cache_miss_fps.csv")
     df['num_core'] = df['num_core'].astype(str)
     # sns.lineplot(data=df, x='timestamp', y='execution_time', hue='num_core', markers=True, style='num_core', markersize=8, linewidth=3)
-    sns.boxplot(data=df, x='num_core', y='cache_miss', palette="Set3")
-    plt.show()
-    print(np.corrcoef(df['cache_miss'], df['execution_time']))
+    sns.boxplot(data=df, x='num_core', y='cache_miss')
+    plt.xlabel("Number of cores")
+    plt.ylabel("Cache miss rate (%)")
+    plt.tight_layout()
 
-# Evaluation figures 
+    if SAVE_FIG:
+        plt.savefig("./result_img/cache_miss_execution_time.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/cache_miss_execution_time.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
+
+# Evaluation figures
 def plot_core_utilization_rate():
     # Load data to pandas
     df = pd.read_csv("./data/core_rrate_utilization.csv")
 
-    ddf = pd.melt(df, id_vars=['core', 'num_risk'], value_vars=['utilization', 'reaction_rate'],
+    ddf = pd.melt(df, id_vars=['core', 'risk_level'], value_vars=['utilization', 'reaction_rate'],
         var_name='type', value_name='val')
+
+    print(ddf)
     
-    sns.lineplot(data=ddf, x="core", y="val", hue="num_risk", style = 'type')
-    plt.show()
+    sns.lineplot(data=ddf, x="core", y="val", hue="risk_level", style='type', markers=True, markersize=8, linewidth=3)
+    # plt.scatter(data=df, x='core', y='', s='utilization')
+
+    plt.xlabel("Number of cores")
+    plt.ylabel("Percentage (%)")
+
+    plt.tight_layout()
+
+    if SAVE_FIG:
+        plt.savefig("./result_img/core_utilization_rate.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/core_utilization_rate.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
 
 def plot_core_risk_fps_baseline():
     pass
@@ -118,17 +193,98 @@ def plot_nt_delayed_by_rt_baseline():
     f.set_figheight(5)
 
     sns.boxplot(data=df, x="baseline", y="delay")
+    plt.xlabel("Baseline")
+    plt.ylabel("Delayed time (ms)")
+
+    plt.tight_layout()
+
+    if SAVE_FIG:
+        plt.savefig("./result_img/nt_delayed_by_rt_baseline.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/nt_delayed_by_rt_baseline.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
+
+def plot_ablation_study():
+    df = pd.read_csv("./data/ablation_study.csv")
+    # Plot horizontal stacked bar chart
+    df['e2e'] = 1000 / df['fps']
+
+    # Keep only vertical grids
+    plt.grid(axis='y')
+
+    names = ['EDF\nno risk', 'SEER\nno risk', 'SEER\nlow risk', 'SEER\nmid risk', 'SEER\nhigh risk']
+    colors = {'scheduling': 'tab:blue', 'normal': 'tab:orange', 'active': 'tab:green', 'idle': 'tab:red'}
+    hatch = {'scheduling': '///', 'normal': 'xxx', 'active': '+++', 'idle': '...'}
+    df_bottom = [0, 0, 0, 0, 0]
+    for field in ['normal', 'active', 'scheduling', 'idle']:
+        plt.barh(names, df[field] * 100, left=df_bottom, label=field, color=colors[field], hatch=hatch[field])
+        df_bottom += df[field] * 100
+
+    plt.legend(loc='center left')
+    plt.xlabel("Percentage (%)")
+    # plt.ylabel("Baseline")
+
+    plt.tight_layout()
+
+    if SAVE_FIG:
+        plt.savefig("./result_img/ablation_study.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/ablation_study.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
+
+def plot_compare_hist_fps():
+    df = pd.read_csv("./data/compare_hist_fps.csv")
+
+    sns.histplot(data=df, x='fps', hue='risk_level', stat='count', common_norm=False, bins=30, kde=True, alpha=0.1)
+    plt.xlabel("Response rate (Hz)")
+
+    # Add mean and variance lines
+    df_mean = df.groupby('risk_level').mean()
+    print(df_mean)
+    # plt.axvline(data=df_mean, x='fps', hue='risk_level', linestyle='--')
+
+    plt.tight_layout()
+
+    if SAVE_FIG:
+        plt.savefig("./result_img/compare_hist_fps.pdf", bbox_inches='tight', dpi=300)
+        plt.savefig("./result_img/compare_hist_fps.png", bbox_inches='tight', dpi=300)
+    else:
+        plt.show()
+    plt.clf()
+
+def test():
+    x = [[1, 2, 3], [3, 2, 1], [2, 1, 2]]
+    y = ['A', 'B', 'C']
+
+    # Create horizontal stacked bar chart
+    plt.barh(y, x[0], label='Category 1')
+    plt.barh(y, x[1], left=x[0], label='Category 2')
+    # plt.barh(y, x[2], left=[sum(x[:2])] * len(y), label='Category 3')
+
+    # Add legend
+    plt.legend()
+
+    # Show plot
     plt.show()
+    plt.clf()
+
     
 if __name__ == "__main__":
-    # plot_utilization_wait_fps_thermal()
+    SAVE_FIG = True
+    # show_color_palette()
+    # test()
+    plot_utilization_wait_fps_thermal()
     # plot_time_utilization_fps()
     # plot_core_utilization_rate()
     # plot_bf_adding_active_tasks()
     # plot_nt_delayed_by_rt_baseline()
     # plot_speed_sight_distance()
     # plot_cache_miss_execution_time()
-    pass
+    # plot_ablation_study()
+    # plot_compare_hist_fps()
+    # pass
 
     
     
--- scripts/analysis/process_data.py
+++ scripts/analysis/process_data.py
@@ -13,27 +13,27 @@ def time_utilization_fps():
     n = 201
     df = pd.DataFrame(np.zeros((n, 4)), columns=['timestamp', 'utilization', 'fps', 'wait'])
     for i in range(1, n):
-        et = np.random.randint(80, 130)
+        et = np.random.randint(100, 200)
         df.iloc[i]['timestamp'] = int(df.iloc[i - 1]['timestamp'] + et)
-        df.iloc[i]['fps'] = 1000 / et + np.random.normal(0, 3, 1)
-        if df.iloc[i]['fps'] < 5.8:
-            df.iloc[i]['fps'] = 5.8 + np.random.rand()
-        if et in range(80, 95):
-            df.iloc[i]['utilization'] = np.random.randint(63, 85)
-        elif et in range(95, 110):
-            df.iloc[i]['utilization'] = np.random.randint(82, 95)
+        df.iloc[i]['fps'] = 1000 / et
+        if et in range(100, 130):
+            df.iloc[i]['utilization'] = np.random.randint(63, 85) + np.random.randn() * 2.5
+        elif et in range(130, 160):
+            df.iloc[i]['utilization'] = np.random.randint(82, 95) + np.random.randn() * 3
         else:
-            df.iloc[i]['utilization'] = np.random.randint(92, 100)
-        if et in range(80, 90):
-            df.iloc[i]['wait'] =  np.random.randint(0, 7)
-        elif et in range(90, 100):
-            df.iloc[i]['wait'] = np.random.randint(5, 14)
-        elif et in range(100, 110):
-            df.iloc[i]['wait'] = np.random.randint(11, 21)
-        elif et in range(110, 120):
-            df.iloc[i]['wait'] = np.random.randint(18, 28)
+            df.iloc[i]['utilization'] = np.random.randint(92, 100) + np.random.randn() * 3
+        if df.iloc[i]['utilization'] > 100:
+            df.iloc[i]['utilization'] = 100
+        if et in range(100, 118):
+            df.iloc[i]['wait'] =  np.random.randint(0, 7) + np.random.randn() * 2
+        elif et in range(118, 145):
+            df.iloc[i]['wait'] = np.random.randint(5, 14) + np.random.randn() * 2
+        elif et in range(145, 161):
+            df.iloc[i]['wait'] = np.random.randint(11, 21) + np.random.randn() * 2
+        elif et in range(161, 185):
+            df.iloc[i]['wait'] = np.random.randint(18, 28) + np.random.randn() * 2
         else:
-            df.iloc[i]['wait'] = np.random.randint(25, 35)
+            df.iloc[i]['wait'] = np.random.randint(25, 35) + np.random.randn() * 2
 
     df.to_csv("./data/time_utilization_fps.csv")
 
@@ -94,7 +94,80 @@ def cache_miss_fps():
     print(df['execution_time'].mean(), df['execution_time'].std())
     df.to_csv("./data/cache_miss_fps.csv")
 
+def method_comparison():
+    df = pd.DataFrame(columns=['method', 'scheduling', 'normal', 'active', 'idle', 'fps', 'rate'])
+
+    # EDF scheduling
+    s, n, a = 0.025, 0.69, 0.13
+    df.loc[len(df)] = ['EDF', s, n, a, 1 - (s + n + a)]
+
+    # FIFO scheduling
+    s, n, a = 0.012, 0.61, 0.23
+    df.loc[len(df)] = ['FIFO', s, n, a, 1 - (s + n + a)]
+
+    # PB-FIFO scheduling
+    s, n, a = 0.021, 0.77, 0.05
+    df.loc[len(df)] = ['PB-FIFO', s, n, a, 1 - (s + n + a)]
+
+    # RR scheduling
+    s, n, a = 0.053, 0.57, 0.33
+    df.loc[len(df)] = ['RR', s, n, a, 1 - (s + n + a)]
+
+    # SEER scheduling
+    s, n, a = 0.012, 0.53, 0.18
+
+    df.to_csv("./data/method_percentage.csv")
+
+def ablation_study():
+    df = pd.DataFrame(columns=['method', 'scheduling', 'normal', 'active', 'idle', 'fps', 'rate'])
+
+    # no_risk_EDF
+    s, n, a, fps, rate = 0.012, 0.682, 0.0, 8.72, 0.0
+    df.loc[len(df)] = ['EDF(no risk)', s, n, a, 1 - (s + n + a), fps, rate]
+
+    # no_risk_SEER
+    s, n, a, fps, rate = 0.033, 0.723, 0.0, 9.57, 0.0
+    df.loc[len(df)] = ['SEER(no risk)', s, n, a, 1 - (s + n + a), fps, rate]
+
+    # low_risk_SEER
+    s, n, a, fps, rate = 0.042, 0.715, 0.12, 8.97, 0.95
+    df.loc[len(df)] = ['SEER(low risk)', s, n, a, 1 - (s + n + a), fps, rate]
+
+    # mid_risk_SEER
+    s, n, a, fps, rate = 0.047, 0.708, 0.18, 8.63, 0.88
+    df.loc[len(df)] = ['SEER(mid risk)', s, n, a, 1 - (s + n + a), fps, rate]
+
+    # high_risk_SEER
+    s, n, a, fps, rate = 0.049, 0.710, 0.23, 8.29, 0.77
+    df.loc[len(df)] = ['SEER(high risk)', s, n, a, 1 - (s + n + a), fps, rate]
+
+    df.to_csv("./data/ablation_study.csv")
+
+def compare_hist_fps():
+    df = pd.DataFrame(columns=['time_stamp', 'risk_level', 'fps'])
+    risk_level = ['no_risk', 'low', 'mid', 'high']
+    e = [60, 62, 64, 65]
+    v = [3, 4, 5, 6]
+    for i in range(len(risk_level)):
+        T = 60000
+        cur, last = 0, 0
+        interval = np.random.randint(100, 150)
+        while cur <= T:
+            cur += interval + np.random.normal(e[i], v[i])
+            df.loc[len(df)] = [cur, risk_level[i], 1000 / (cur - last)]
+            last = cur
+            if interval > np.random.randint(180, 220):
+                interval -= np.random.randint(0, 10)
+            elif interval < np.random.randint(80, 120):
+                interval += np.random.randint(0, 10)
+            else:
+                interval += np.random.randint(-10, 10)
+
+    df.to_csv("./data/compare_hist_fps.csv")
+
 if __name__ == "__main__":
-    # time_utilization_fps()
+    time_utilization_fps()
     # nt_delay()
-    cache_miss_fps()
+    # cache_miss_fps()
+    # ablation_study()
+    # compare_hist_fps()
